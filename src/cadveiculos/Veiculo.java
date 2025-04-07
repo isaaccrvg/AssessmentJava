cc098f043600adba74f602d243261fcc882c85d2
isaac <isaaccrvg98@hotmail.com>
2025-04-06 22:21:40 -0300

    Exercício 6 Cadastro de Veículos

--- src/cadveiculos/Veiculo.java
+++ src/cadveiculos/Veiculo.java
@@ -1,4 +1,34 @@
 package cadveiculos;
 
 public class Veiculo {
+    private String placa;
+    private String modelo;
+    private int fabricacao;
+    private double quilometragem;
+
+    public Veiculo(String placa, String modelo, int fabricacao, double quilometragem) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.fabricacao = fabricacao;
+        this.quilometragem = quilometragem;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void exibirDetalhes(){
+        System.out.println("Placa: " + placa);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano de fabricação: " + fabricacao);
+        System.out.println("Quilometragem: " + quilometragem + "km");
+    }
+
+    public void registrarViagem(double km){
+        if (km > 0){
+            quilometragem += km;
+        }else{
+            System.out.println("Quilometragem inválida, informe um valor acima de 0!");
+        }
+    }
 }
--- src/cadveiculos/mainVeiculos.java
+++ src/cadveiculos/mainVeiculos.java
@@ -1,4 +1,16 @@
 package cadveiculos;
 
 public class mainVeiculos {
+    public static void main(String[] args) {
+        Veiculo v1 = new Veiculo("kzo-5228", "Civic", 2013, 207000);
+        Veiculo v2 = new Veiculo("zno-2025", "Corolla", 2011, 12000);
+
+        v1.registrarViagem(100);
+        v2.registrarViagem(30.0);
+
+        v1.exibirDetalhes();
+        System.out.println("=====");
+        v2.exibirDetalhes();
+
+    }
 }
